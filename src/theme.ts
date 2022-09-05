33f62ec47074bf2def5a7ea2435ac0891a44edfe
이동형 <leedhhhhh@gmail.com>
2022-09-05 17:01:25 +0900

    style 추가

--- src/theme.ts
+++ src/theme.ts
@@ -4,4 +4,14 @@ export const darkTheme: DefaultTheme = {
   bgColor: "#3F8CF2",
   boardColor: "#DADFE9",
   cardColor: "white",
+  bgGradient:
+    "linear-gradient(108deg, #feac5e, #fcaa62 12%, #df909d 33%, #c779d0 51%, #a38ece 70%, #859fcc 86%, #4bc0c8)",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#4bc0c8",
+  boardColor: "#ecf0f1",
+  cardColor: "white",
+  bgGradient:
+    "linear-gradient(108deg, #feac5e, #fcaa62 12%, #df909d 33%, #c779d0 51%, #a38ece 70%, #859fcc 86%, #4bc0c8)",
 };
