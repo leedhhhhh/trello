540e845c1deb34211f03d1053182cdca832ef735
이동형 <leedhhhhh@gmail.com>
2022-09-05 17:00:33 +0900

    state atom 추가

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -12,8 +12,48 @@ interface IToDoState {
 export const toDoState = atom<IToDoState>({
   key: "toDo",
   default: {
-    "To Do": [],
-    Doing: [],
-    Done: [],
+    Board: [],
   },
+  effects: [
+    ({ setSelf, onSet }) => {
+      const todoStoreKey = "Todo";
+      const savedValue = localStorage.getItem(todoStoreKey);
+      // setSelf() 함수 내에서는 Promise를 사용하거나 데이터를 비동기적으로 호출할 때 사용할 수 있다.
+      if (savedValue !== null) {
+        setSelf(JSON.parse(savedValue));
+      }
+
+      onSet((newValue, _, isReset) => {
+        isReset
+          ? localStorage.removeItem(todoStoreKey)
+          : localStorage.setItem(todoStoreKey, JSON.stringify(newValue));
+      });
+    },
+  ],
+});
+
+export const boardState = atom<string[]>({
+  key: "boards",
+  default: ["Board"],
+  effects: [
+    ({ setSelf, onSet }) => {
+      const todoStoreKey = "Todo";
+      const savedValue = localStorage.getItem(todoStoreKey);
+      // setSelf() 함수 내에서는 Promise를 사용하거나 데이터를 비동기적으로 호출할 때 사용할 수 있다.
+      if (savedValue !== null) {
+        setSelf(JSON.parse(savedValue));
+      }
+
+      onSet((newValue, _, isReset) => {
+        isReset
+          ? localStorage.removeItem(todoStoreKey)
+          : localStorage.setItem(todoStoreKey, JSON.stringify(newValue));
+      });
+    },
+  ],
+});
+
+export const darkState = atom({
+  key: "isDark",
+  default: false,
 });
